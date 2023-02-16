b11c1b6192fac49377e9132e28da928f5f221a1e
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-02-16 20:03:15 -0300

    26/02/23

--- Operacoes/App.java
+++ Operacoes/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         Operadores objeto1 = new Operadores();
         objeto1.aritmetica();
+        objeto1.atribuicao();
 
     }
 }
--- Operacoes/Operadores.java
+++ Operacoes/Operadores.java
@@ -19,6 +19,33 @@ public class Operadores {
                 + moduloOuResto);
         System.out.println("Fim!");
         System.out.println("===============================");
-
     }
+    public void atribuicao() {
+        System.out.println("===============================");
+        int umNumero = 5;
+        umNumero += 2;
+        System.out.println("Resultado da adição de 5 com 2: " + umNumero);
+        umNumero = 5; // Voltando o valor para 5.
+        umNumero -= 2;
+        System.out.println("Resultado da subtração de 5 pelo 2: "
+        + umNumero);
+        umNumero = 5; // Voltando o valor para 5.
+        umNumero *= 2;
+        System.out.println("Resultado da multiplicação de 5 pelo 2: "
+        + umNumero);
+        umNumero = 5; // Voltando o valor para 5.
+        umNumero /= 2;
+        System.out.println("Resultado da divisão de 5 pelo 2: "
+        + umNumero);
+        umNumero = 5; // Voltando o valor para 5.
+        umNumero %= 2;
+        System.out.println("Resto da divisão de 5 pelo 2: " + umNumero);
+        System.out.println("Fim!");
+        System.out.println("===============================");
+    }
+        
+    }
+
+}
+
 }
