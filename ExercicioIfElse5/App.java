b5a767cb0f97869765fb6904f27c711c7196edd3
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-03-06 20:00:12 -0300

    06/03/23

---
+++ ExercicioIfElse5/App.java
@@ -0,0 +1,11 @@
+package ExercicioIfElse5;
+
+public class App {
+    
+    public static void main(String[] args) {
+
+        Exercicio5 objeto1 = new Exercicio5();
+        objeto1.exercicio5();
+}
+
+}
