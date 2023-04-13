a693226a15f749544cb9c8663ef4dc678963643e
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-04-13 20:42:12 -0300

    13/04/23 - vetores + SwitchCase + While

--- VetoresArray4/Exercicio4.java
+++ VetoresArray4/Exercicio4.java
@@ -11,14 +11,43 @@ public class Exercicio4 {
     int i=0;
     int cont = 0; //contador para as consoantes
     while(i<letras.length){
-        if(letras[i]=="a"||letras[i]=="e"||letras[i]=="i"
+        if(letras[i]=="a"||letras[i]=="e"||letras[i]=="i"  // "||" condição   "OU"
         ||letras[i]=="o"||letras[i]=="u"){
 
         }else{
-            cont++;
+            cont++; //conta + 1 no contador
             System.out.println(letras[i]+" é consoante");
         }
+        i++; // conta + 1 na variavel "i"
         }
         System.out.println("o nº de consoantes é "+cont);
+        }
+
+
+
+        public void exemploSthCase(){
+            String letras[]= new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"};
+    
+        int i=0;
+        int cont = 0;
+        int vogal=0;
+        while(i<letras.length){
+            switch(letras[i]){
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                vogal++;
+                System.out.println(letras[i]+ " é vogal");
+                break;
+                default: cont++;
+                System.out.println(letras[i]+" é Consoante");
+            }
+            i++;
+        }
+        System.out.println("o nº de vogais é "+vogal);
+        System.out.println("o nº de consoantes é "+cont);
+
         }
     }
