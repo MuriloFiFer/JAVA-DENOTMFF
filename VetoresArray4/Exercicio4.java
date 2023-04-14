47c59c8ef079c3d18fa29cd657f23052880c2c82
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-04-13 21:33:35 -0300

    13/04/23

--- VetoresArray4/Exercicio4.java
+++ VetoresArray4/Exercicio4.java
@@ -48,6 +48,53 @@ public class Exercicio4 {
         }
         System.out.println("o nº de vogais é "+vogal);
         System.out.println("o nº de consoantes é "+cont);
-
         }
-    }
+
+        public void exemplo5(){
+            int vetor[]=new int []{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+            int i=0;
+            int contPar=0;
+            int contImpar=0;
+            while(i<vetor.length){  //length = tamanho  (tamanho do vetor)
+                if(vetor[i]%2==0){
+                    contPar++;
+                }else{ // caso contrario
+                    contImpar++;
+                }
+                i++;
+            }
+            int vetorPar[]=new int[contPar];
+            int vetorImpar[]=new int[contImpar];
+            i=0;
+            contPar=0;
+            contImpar=0;
+            while(i<vetor.length){
+                if(vetor[i]%2==0){
+                    vetorPar[contPar]=vetor[i];
+                    contPar++;
+                }else{ // caso contrario
+                    vetorImpar[contImpar]=vetor[i];
+                    contImpar++;
+                }
+                i++;
+            }
+            i=0;
+            while(i<vetor.length){   
+                System.out.println("vetor["+i+"]="+vetor[i]);
+                i++;
+                }
+            i=0;
+            while(i<vetorPar.length){   
+            System.out.println("vetorPar["+i+"]="+vetorPar[i]);
+            i++;
+            }
+            i=0;
+            while(i<vetorImpar.length){
+            System.out.println("vetorImpar["+i+"]="+vetorImpar[i]);
+            i++;
+            }
+            
+        }
+    
+
+}
