3d8e83c6927d8530a1bebf738214eb0d42507afe
MuriloFiFer <123783941+MuriloFiFer@users.noreply.github.com>
2023-04-25 22:36:53 +0000

    25/04/23

--- ExerciciosVetores/ExercicioVetores.java
+++ ExerciciosVetores/ExercicioVetores.java
@@ -115,5 +115,22 @@ public void Exercicio6(){
         System.out.println(vetorc[8]);
         System.out.println(vetorc[9]);
 }
+
+public void Exercicio7(){
+    // criar vetor
+    int vetor[]=new int[]{1,2,3,5,7};
+    //percorrer o vetor e realizae as operações(+ e *)
+    int soma = 0;  // acumular a soma dos elementos do vetor
+    int mult = 1;  //acumular a multiplicação dos elementos 
+    for (int i = 0; i < vetor.length; i++) {
+        soma+=vetor[i];  //acumula a soma
+        mult*=vetor[i];  //acumula a multiplicação
+    }
+    System.out.println("O valor da Soma do vetor é "+soma);
+    System.out.println("O valor da Multiplicação do vetor é "+mult);
+    for (int i = 0; i < vetor.length; i++) {
+        System.out.println("vetor["+i+"]="+vetor[i]);
+    }
+}
 }
 
