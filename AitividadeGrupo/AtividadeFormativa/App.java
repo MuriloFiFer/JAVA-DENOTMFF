0972a7086b97f4984dcc2de77e92a0686a5f5d00
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-06-06 21:10:04 -0300

    06/06/23

--- AitividadeGrupo/AtividadeFormativa/App.java
+++
@@ -1,9 +0,0 @@
-package AitividadeGrupo.AtividadeFormativa;
-
-public class App {
-    public static void main(String[]args){
-        Atividade obj = new Atividade(); // nome da classe
-        obj.exercicio3(); //nome do metodo void
-    }
-
-}
