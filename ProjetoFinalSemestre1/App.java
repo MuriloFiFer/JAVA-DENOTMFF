6452b912b0c06106e587887110e3eadc8fa04af3
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2023-06-06 22:01:09 -0300

    06/06/23

---
+++ ProjetoFinalSemestre1/App.java
@@ -0,0 +1,5 @@
+package ProjetoFinalSemestre1;
+
+public class App {
+    
+}
---
+++ ProjetoFinalSemestre1/Banco.java
@@ -0,0 +1,9 @@
+package ProjetoFinalSemestre1;
+
+public class Banco {
+    int operacao;
+    String conta;
+    int Nconta;
+      
+
+}
